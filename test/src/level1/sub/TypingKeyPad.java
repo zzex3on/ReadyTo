1e844daab4d3b1aad5688c45859d5f76596eb29e
zzex3on <zzeonii0612@gmail.com>
2021-08-29 23:48:27 +0900

    - 키패드 누르기 왼/오 구별 진행

--- test/src/level1/sub/TypingKeyPad.java
+++ test/src/level1/sub/TypingKeyPad.java
@@ -4,23 +4,41 @@ public class TypingKeyPad {
 	public String solution(int[] numbers, String hand) {
 		String answer = "";
 		char keyPad[][] = {{'1','2','3'},{'4','5','6'},{'7','8','9'},{'*','0','#' }}; // 숫자 키패드
+		char leftJ = '*'; // 왼쪽 손가락 위치 (j의 좌표값)
+		char rightJ = '#'; // 오른쪽 손가락 위치 (j의 좌표값)
 		
 		for (int i = 0; i < numbers.length; i++) {
-			char key = (char) numbers[i];
+			char key = Character.forDigit(numbers[i], 10);
 			for(int j = 0; j < keyPad.length; j++) {
 				for(int k = 0; k <keyPad[j].length; k++) {
-					if(key == keyPad[j][k]) {
-						if(k == 0) {			// 왼손
-							
-						} else if(k == 2) {		// 오른손
-							
-						} else {				// 그 외
-							// hand 고려하여 코드 작성
+					if(Character.compare(key, keyPad[j][k]) == 0) {
+						switch(k) {
+						case 0:
+							answer += "L";
+							leftJ = Character.forDigit(j, 10);
+							break;
+						case 2:
+							answer += "R";
+							rightJ = Character.forDigit(j, 10);
+							break;
+						default:
+							// 왼/오 구별
+							// 어디 손 잡이인지를 먼저 구별하도록 로직 변경하기
+							if((Math.abs(j - Character.getNumericValue(leftJ)) - Math.abs(j - Character.getNumericValue(rightJ))) < 0) {
+								answer += "L";
+								break;
+							} else if((Math.abs(j - Character.getNumericValue(leftJ)) - Math.abs(j - Character.getNumericValue(rightJ))) > 0) {
+								answer += "R";
+								break;
+							} else {
+								answer += hand.substring(1).toUpperCase();
+								break;
+							}
 						}
 					}
 				}
+				System.out.println("answer : " + answer);
 			}
-			  
 		}
 		
 		
